9764546c77479c0481af2d54142f5e672294857f
jbground <jsj00203@gmail.com>
2024-03-19 20:25:41 +0900

    initialize

---
+++ src/main/java/com/jbground/order/YouthServiceOrderApplication.java
@@ -0,0 +1,13 @@
+package com.jbground.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthServiceOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouthServiceOrderApplication.class, args);
+	}
+
+}
